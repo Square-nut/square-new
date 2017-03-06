112a9e037a06ae9c5c8d0562e81722605104bc17
Square-nut <zhenjiaguwen123@gmail.com>
2017-03-06 18:55:20 +0800

    modify file

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
+import Login_main from 'views/Login-main';
+import Login from 'components/Login';
+import Register from 'components/Register';
+import recharge from 'views/recharge-main'
+
+import Nav from 'components/Nav'
+import Home from 'views/Home'
+
+import VirtualQQ from 'components/virtualQQ_nav'
+import QQ_setting from 'views/virtualQQ/setting'
+import QQ_home from 'views/virtualQQ/talkList'
+import QQ_talk from 'views/virtualQQ/talking'
+import QQ_contacts from 'views/virtualQQ/contacts'
+import QQ_active from 'views/virtualQQ/active'
+import Recharge from 'views/recharge-main.vue'
+import Myself from 'views/Myself'
 Vue.use(Router)
 
 export default new Router({
