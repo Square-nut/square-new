7906a12e34e8642201db50f1ba5a3b2280aaee1e
Square-nut <zhenjiaguwen123@gmail.com>
2017-03-07 20:28:51 +0800

    go home

---
+++ src/components/Login-main.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="bg">
+		<div class="loginContainer">
+			<nav>
+				<router-link to='/login' :class='{current:true}'>登录</router-link>
+				<router-link to='/register'>注册</router-link>
+			</nav>
+			<div class="login-wrap">
+				<router-view></router-view>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				isLogin:false,
+				setCurrentIndex:0
+			}
+		}
+//		,
+//		watch:{
+//			// 如果路由有变化，会再次执行该方法 只要地址变化，就会执行这个方法'$route':'getNavData'
+//			'$route' (to, from) {
+//				this.setCurrentIndex(to);
+//			}
+//		}
+	}
+</script>
+<style scoped>
+	.bg{
+		width: 100%;
+		height: 100%;
+		
+	}
+	nav a {
+		display: inline-block;
+		height: 80px;
+		line-height: 80px;
+		padding: 0 32px 0;
+		text-decoration: none;
+	}
+	nav a .current{
+		color: #108ee9;
+		border-bottom: 4px solid;
+	}
+	.loginContainer{
+		width: 50rem;
+		height: 50rem;
+	}
+	.bar{
+		color: #C9E6F2;
+		border: 1px solid;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,30 +2,58 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
-import Login_main from 'views/Login-main';
-import Login from 'components/Login';
-import Register from 'components/Register';
-import recharge from 'views/recharge-main'
+import Login_main from '@/components/Login-main';
+import Login from '@/components/Login';
+import Register from '@/components/Register';
+//import Recharge from '@/components/recharge-main.vue'
 
-import Nav from 'components/Nav'
-import Home from 'views/Home'
+import Nav from '@/components/Nav'
+import Home from '@/components/Home'
 
-import VirtualQQ from 'components/virtualQQ_nav'
-import QQ_setting from 'views/virtualQQ/setting'
-import QQ_home from 'views/virtualQQ/talkList'
-import QQ_talk from 'views/virtualQQ/talking'
-import QQ_contacts from 'views/virtualQQ/contacts'
-import QQ_active from 'views/virtualQQ/active'
-import Recharge from 'views/recharge-main.vue'
-import Myself from 'views/Myself'
-Vue.use(Router)
+import VirtualQQ from '@/components/virtualQQ_nav'
+import QQ_setting from '@/components/virtualQQ/setting'
+import QQ_home from '@/components/virtualQQ/talkList'
+import QQ_talk from '@/components/virtualQQ/talking'
+import QQ_contacts from '@/components/virtualQQ/contacts'
+import QQ_active from '@/components/virtualQQ/active'
+import Myself from '@/components/Myself'
+Vue.use(Router);
 
+//export default new Router({
+//routes: [
+//  {
+//    path: '/',
+//    name: 'Hello',
+//    component: Hello
+//  }
+//]
+//})
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
-})
+	routes:[{
+		path:"/",
+		component: Login_main,
+		children:[
+			{path:'login',component:Login},
+			{path:'register',component:Register}
+		]
+	},{
+		path:'/home',
+		component:Nav,
+		children:[
+			{path:'main',name:'main',component: Home},
+			{
+				path:'qq',
+				name:'qq',
+				component: VirtualQQ,
+				children:[
+					{path:'setting',name:'setting',component: QQ_setting},
+					{path:'talk',name:'talk',component: QQ_talk},
+					{path:'contacts',name:'contacts',component: QQ_contacts},
+					{path:'active',name:'active',component: QQ_active}
+				]
+			},
+//			{path:'recharge',name:'recharge',component: Recharge},
+			{path:'self',name:'self',component: Myself}
+		]
+	}]
+});
