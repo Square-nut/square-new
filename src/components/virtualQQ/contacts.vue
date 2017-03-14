cb712775c0c2bc5b67fd7cdf171a3224c430025a
Square-nut <zhenjiaguwen123@gmail.com>
2017-03-14 18:37:41 +0800

    123

---
+++ src/components/virtualQQ/contacts.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="container">
+	</div>
+</template>
+<script>
+	export default {
+		
+	}
+</script>
+<style scoped>
+	.w100{
+		width: 100%;
+	}
+	.w90{
+		width: 90%;
+	}
+</style>
---
+++ src/components/virtualQQ/register.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="container">
+	</div>
+</template>
+<script>
+	export default {
+	}
+</script>
+<style scoped>
+	.w100{
+		width: 100%;
+	}
+	.w90{
+		width: 90%;
+	}
+</style>
